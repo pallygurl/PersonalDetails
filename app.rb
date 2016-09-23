081e6866962b98d135f6a2c31826ae2cb06a8255
Cyndi Bolton <cbolton@wvtpg.com>
2016-09-23 12:08:17 -0400

    finished with first part

--- app.rb
+++ app.rb
@@ -5,32 +5,35 @@ get '/' do
 end
 
 post '/getname' do
-	name = params[:user_name]
-	redirect '/age?users_name=' + name
+	yourname = params[:user_name]
+	redirect '/age?users_name=' + yourname
 end	
 
 get '/age' do
-	name = params[:users_name]
-	erb :getage, :locals => {:name => name}
+	yourname = params[:users_name]
+	erb :getage, :locals => {:yourname => yourname}
 end
 
 post '/getage' do
 	age = params[:user_age]
-	name = params[:user_name]
-	redirect '/numbers?user_name=' + name + "&user_age=" + age
+	yourname = params[:user_name]
+	redirect '/numbers?user_name=' + yourname + "&user_age=" + age
 end
 
 get '/numbers' do
 	age = params[:user_age]
-	name = params[:user_name]
-	erb :getnumbers, :locals => {:name => name, :age => age}
+	yourname = params[:user_name]
+	erb :getnumbers, :locals => {:yourname => yourname, :age => age}
 end
 
 post '/getnumbers' do
 	age = params[:user_age]
-	name = params[:user_name]
-	number1 = params[:fav_1]
-	number2 = params[:fav_2]
-	number3 = params[:fav_3]
+	yourname = params[:user_name]
+	num1 = params[:fav_1]
+	num2 = params[:fav_2]
+	num3 = params[:fav_3]
+	sum = num1 + num2 + num3
+
+		
+"So your name is #{yourname}, your age is #{age}, and your 3 favorite numbers are #{num1}, #{num2}, and #{num3}."
 end
-	
